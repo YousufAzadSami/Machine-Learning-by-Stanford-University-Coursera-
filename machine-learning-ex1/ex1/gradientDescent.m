73cbdda04ff3912709e627c1eb19c63428d7c93f
sami <sami1592@gmail.com>
2017-12-28 22:04:31 +0100

    gradient descent was updating simultaneously, fixed that

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,8 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    theta_temp = theta;
     for theta_iter = 1 : size(theta,1)
-      theta(theta_iter) = theta(theta_iter) - alpha / m * sum(((X*theta .- y) .* X(:, theta_iter)));
+      theta(theta_iter) = theta(theta_iter) - alpha / m * sum(((X*theta_temp .- y) .* X(:, theta_iter)));
     end
     % ============================================================
 
