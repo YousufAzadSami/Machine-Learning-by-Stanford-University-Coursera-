e7392e6870e1be16f0a053d674e8d6445138e2d9
Yousuf Azad (Sami) <sami1592@gmail.com>
2018-07-02 23:52:18 +0200

    Email preprocessing

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -97,15 +97,14 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
-
+    for i = 1:length(vocabList) 
+        word = vocabList{i}; 
+        if (strcmp(str, word) == 1) 
+          word_indices = [word_indices ; i]  
+        end 
+    endfor 
+ 
+ 
     % =============================================================
 
 
